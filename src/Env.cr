b8b2c18ca841abc7f5cdb73bd23d5a8e650dcc67
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-08-26 16:05:35 +0300

    move ops registration to Env

--- src/Env.cr
+++ src/Env.cr
@@ -5,21 +5,21 @@ require "./common.cr"
 require "./Log.cr"
 
 module Ph
+  enum OpT : UInt8
+    DELETE_KEY       = 0
+    DELETE_VALUE     = 1
+    DELETE_KEY_VALUE = 2
+    INSERT           = 3
+    UPDATE           = 4
+  end
+
+  alias Op = {K, Nil} |
+             {Nil, V} |
+             { {K, V}, Nil } |
+             {K, V} |
+             { {K, V}, {K, V} }
+
   class Tx
-    enum OpT : UInt8
-      DELETE_KEY       = 0
-      DELETE_VALUE     = 1
-      DELETE_KEY_VALUE = 2
-      INSERT           = 3
-      UPDATE           = 4
-    end
-
-    alias Op = {K, Nil} |
-               {Nil, V} |
-               { {K, V}, Nil } |
-               {K, V} |
-               { {K, V}, {K, V} }
-
     getter ops : Array(Op) = [] of Op
 
     protected def initialize(@env : Env)
@@ -76,136 +76,30 @@ module Ph
         case op
         when {K, Nil}
           k = op[0].as K
-          ::Log.debug { "commit delete\n" + " " * 37 + "[#{k.hexstring}, *]" }
-
           buf.write_byte OpT::DELETE_KEY.value
           Ph.write buf, k
-
-          @env.dK << k
-
-          @env.unk[k].each do |v, kvs|
-            @env.unv[v].delete k
-            kvs.each do |kv|
-              @env.uk[kv[0]].not_nil!.delete kv[1]
-              @env.uv[kv[1]].not_nil!.delete kv[0]
-            end
-          end rescue nil
-          @env.unk.delete k
-
-          @env.ik[k].each { |v| @env.iv.delete v }
-          @env.ik.delete k
         when {Nil, V}
           v = op[1].as V
-          ::Log.debug { "commit delete\n" + " " * 37 + "[*, #{v.hexstring}]" }
-
           buf.write_byte OpT::DELETE_VALUE.value
           Ph.write buf, v
-
-          @env.dV << v
-
-          @env.unv[v].each do |k, kvs|
-            @env.unk[k].delete v
-            kvs.each do |kv|
-              @env.uk[kv[0]].not_nil!.delete kv[1]
-              @env.uv[kv[1]].not_nil!.delete kv[0]
-            end
-          end rescue nil
-          @env.unv.delete v
-
-          @env.iv[v].each { |k| @env.ik.delete k }
-          @env.iv.delete v
         when { {K, V}, Nil }
           k, v = op[0].as {K, V}
-          ::Log.debug { "commit delete\n" + " " * 37 + "[#{k.hexstring}, #{v.hexstring}]" }
-
-          unless (@env.dK.includes? k) || (@env.dV.includes? v)
-            @env.dk[k] = Set(V).new unless @env.dk.has_key? k
-            @env.dk[k] << v
-
-            @env.dv[v] = Set(K).new unless @env.dv.has_key? v
-            @env.dv[v] << k
-          end
-
           buf.write_byte OpT::DELETE_KEY_VALUE.value
           Ph.write buf, k, v
-          @env.uk[k] = Hash(V, {K, V}?).new unless @env.uk.has_key? k
-          @env.uk[k].not_nil![v] = nil
-
-          @env.uv[v] = Hash(K, {K, V}?).new unless @env.uv.has_key? v
-          @env.uv[v].not_nil![k] = nil
-
-          if (@env.unk.has_key? k) && (@env.unk[k].has_key? v)
-            @env.unk[k][v].each do |ok, ov|
-              @env.uk[ok] = Hash(V, {K, V}?).new unless @env.uk.has_key? ok
-              @env.uk[ok].not_nil![ov] = nil
-              @env.unk[k].not_nil!.delete v rescue nil
-
-              @env.uv[ov] = Hash(K, {K, V}?).new unless @env.uv.has_key? ov
-              @env.uv[ov].not_nil![ok] = nil
-              @env.unv[v].not_nil!.delete k rescue nil
-            end
-          end
-
-          @env.iv[v].delete k rescue nil
-          @env.ik[k].delete v rescue nil
         when {K, V}
           k, v = op[0].as(K), op[1].as(V)
-          ::Log.debug { "commit insert\n" + " " * 37 + "[#{k.hexstring}, #{v.hexstring}]" }
-
           buf.write_byte OpT::INSERT.value
           Ph.write buf, k, v
-
-          @env.ik[k] = Set(V).new unless @env.ik.has_key? k
-          @env.ik[k] << v
-
-          @env.iv[v] = Set(K).new unless @env.iv.has_key? v
-          @env.iv[v] << k
         when { {K, V}, {K, V} }
           k, v = op[0].as {K, V}
           nk, nv = op[1].as {K, V}
-          ::Log.debug { "commit update\n" + " " * 37 + "[#{k.hexstring}, #{v.hexstring}] ->\n" + " " * 37 + "[#{nk.hexstring}, #{nv.hexstring}]" }
-
           buf.write_byte OpT::UPDATE.value
           Ph.write buf, k, v
           Ph.write buf, nk, nv
-
-          @env.uk[k] = Hash(V, {K, V}?).new unless @env.uk.has_key? k
-          @env.uk[k].not_nil![v] = {nk, nv}
-
-          @env.uv[v] = Hash(K, {K, V}?).new unless @env.uv.has_key? v
-          @env.uv[v].not_nil![k] = {nk, nv}
-
-          @env.unk[nk] = Hash(V, Set({K, V})).new unless @env.unk.has_key? nk
-          @env.unk[nk].not_nil![nv] = Set({K, V}).new unless @env.unk[nk].has_key? nv
-          @env.unk[nk].not_nil![nv] << {k, v}
-
-          @env.unv[nv] = Hash(K, Set({K, V})).new unless @env.unv.has_key? nv
-          @env.unv[nv].not_nil![nk] = Set({K, V}).new unless @env.unv[nv].has_key? nk
-          @env.unv[nv].not_nil![nk] << {k, v}
-
-          if (@env.unk.has_key? k) && (@env.unk[k].has_key? v)
-            @env.unk[k][v].each do |ok, ov|
-              @env.uk[ok].not_nil![ov] = {nk, nv}
-              @env.uv[ov].not_nil![ok] = {nk, nv}
-              @env.unk[nk][nv] << {ok, ov}
-              @env.unv[nv][nk] << {ok, ov}
-            end
-            @env.unk[k].delete v
-            @env.unv[v].delete k
-          end
-
-          if (@env.ik.has_key? k) && (@env.ik[k].includes? v)
-            @env.ik[k].delete v
-            @env.ik[nk] = Set(V).new unless @env.ik.has_key? nk
-            @env.ik[nk] << nv
-
-            @env.iv[v].delete k
-            @env.iv[nv] = Set(K).new unless @env.iv.has_key? nv
-            @env.iv[nv] << nk
-          end
         else
           raise "can not commit #{op} of type #{typeof(op)}"
         end
+        @env.register op
       end
       ::Log.debug { "dump transaction to log" }
       @env.log.write buf.to_slice
@@ -229,6 +123,127 @@ module Ph
     getter dV = Set(V).new
     getter dk = Hash(K, Set(V)).new
     getter dv = Hash(V, Set(K)).new
+
+    def register(op : Op)
+      case op
+      when {K, Nil}
+        k = op[0].as K
+        ::Log.debug { "register delete\n" + " " * 37 + "[#{k.hexstring}, *]" }
+
+        @dK << k
+
+        @unk[k].each do |v, kvs|
+          @unv[v].delete k
+          kvs.each do |kv|
+            @uk[kv[0]].not_nil!.delete kv[1]
+            @uv[kv[1]].not_nil!.delete kv[0]
+          end
+        end rescue nil
+        @unk.delete k
+
+        @ik[k].each { |v| @iv.delete v }
+        @ik.delete k
+      when {Nil, V}
+        v = op[1].as V
+        ::Log.debug { "register delete\n" + " " * 37 + "[*, #{v.hexstring}]" }
+
+        @dV << v
+
+        @unv[v].each do |k, kvs|
+          @unk[k].delete v
+          kvs.each do |kv|
+            @uk[kv[0]].not_nil!.delete kv[1]
+            @uv[kv[1]].not_nil!.delete kv[0]
+          end
+        end rescue nil
+        @unv.delete v
+
+        @iv[v].each { |k| @ik.delete k }
+        @iv.delete v
+      when { {K, V}, Nil }
+        k, v = op[0].as {K, V}
+        ::Log.debug { "register delete\n" + " " * 37 + "[#{k.hexstring}, #{v.hexstring}]" }
+
+        unless (@dK.includes? k) || (@dV.includes? v)
+          @dk[k] = Set(V).new unless @dk.has_key? k
+          @dk[k] << v
+
+          @dv[v] = Set(K).new unless @dv.has_key? v
+          @dv[v] << k
+        end
+
+        @uk[k] = Hash(V, {K, V}?).new unless @uk.has_key? k
+        @uk[k].not_nil![v] = nil
+
+        @uv[v] = Hash(K, {K, V}?).new unless @uv.has_key? v
+        @uv[v].not_nil![k] = nil
+
+        if (@unk.has_key? k) && (@unk[k].has_key? v)
+          @unk[k][v].each do |ok, ov|
+            @uk[ok] = Hash(V, {K, V}?).new unless @uk.has_key? ok
+            @uk[ok].not_nil![ov] = nil
+            @unk[k].not_nil!.delete v rescue nil
+
+            @uv[ov] = Hash(K, {K, V}?).new unless @uv.has_key? ov
+            @uv[ov].not_nil![ok] = nil
+            @unv[v].not_nil!.delete k rescue nil
+          end
+        end
+
+        @iv[v].delete k rescue nil
+        @ik[k].delete v rescue nil
+      when {K, V}
+        k, v = op[0].as(K), op[1].as(V)
+        ::Log.debug { "register insert\n" + " " * 37 + "[#{k.hexstring}, #{v.hexstring}]" }
+
+        @ik[k] = Set(V).new unless @ik.has_key? k
+        @ik[k] << v
+
+        @iv[v] = Set(K).new unless @iv.has_key? v
+        @iv[v] << k
+      when { {K, V}, {K, V} }
+        k, v = op[0].as {K, V}
+        nk, nv = op[1].as {K, V}
+        ::Log.debug { "register update\n" + " " * 37 + "[#{k.hexstring}, #{v.hexstring}] ->\n" + " " * 37 + "[#{nk.hexstring}, #{nv.hexstring}]" }
+
+        @uk[k] = Hash(V, {K, V}?).new unless @uk.has_key? k
+        @uk[k].not_nil![v] = {nk, nv}
+
+        @uv[v] = Hash(K, {K, V}?).new unless @uv.has_key? v
+        @uv[v].not_nil![k] = {nk, nv}
+
+        @unk[nk] = Hash(V, Set({K, V})).new unless @unk.has_key? nk
+        @unk[nk].not_nil![nv] = Set({K, V}).new unless @unk[nk].has_key? nv
+        @unk[nk].not_nil![nv] << {k, v}
+
+        @unv[nv] = Hash(K, Set({K, V})).new unless @unv.has_key? nv
+        @unv[nv].not_nil![nk] = Set({K, V}).new unless @unv[nv].has_key? nk
+        @unv[nv].not_nil![nk] << {k, v}
+
+        if (@unk.has_key? k) && (@unk[k].has_key? v)
+          @unk[k][v].each do |ok, ov|
+            @uk[ok].not_nil![ov] = {nk, nv}
+            @uv[ov].not_nil![ok] = {nk, nv}
+            @unk[nk][nv] << {ok, ov}
+            @unv[nv][nk] << {ok, ov}
+          end
+          @unk[k].delete v
+          @unv[v].delete k
+        end
+
+        if (@ik.has_key? k) && (@ik[k].includes? v)
+          @ik[k].delete v
+          @ik[nk] = Set(V).new unless @ik.has_key? nk
+          @ik[nk] << nv
+
+          @iv[v].delete k
+          @iv[nv] = Set(K).new unless @iv.has_key? nv
+          @iv[nv] << nk
+        end
+      else
+        raise "can not commit #{op} of type #{typeof(op)}"
+      end
+    end
 
     def after_initialize
     end
