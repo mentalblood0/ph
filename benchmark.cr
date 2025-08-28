1e7550b86c133fd1f8ecd86657969b90839e11d1
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-08-28 13:13:57 +0300

    update benchmark

--- benchmark.cr
+++ benchmark.cr
@@ -1,73 +1,26 @@
+require "log"
+
 require "./src/Env.cr"
 
-lib LibC
-  TIOCGWINSZ = 0x5413u32
-
-  struct Winsize
-    ws_row : UInt16
-    ws_col : UInt16
-    ws_xpixel : UInt16
-    ws_ypixel : UInt16
-  end
-
-  fun ioctl(fd : LibC::Int, what : LibC::ULong, ...) : LibC::Int
-end
-
-def getWinSize : Tuple(UInt16, UInt16)
-  thing = LibC::Winsize.new
-  LibC.ioctl(STDOUT.fd, LibC::TIOCGWINSZ, pointerof(thing))
-  {thing.ws_row, thing.ws_col}
-end
-
-height, width = getWinSize
-
 env_text = File.read "env.yml"
-ph = Ph::Env.from_yaml env_text
+env = Ph::Env.from_yaml env_text
 
 conf_text = File.read "benchmark.yml"
 conf = NamedTuple(amount: UInt64, key_size: UInt64, value_size: UInt64).from_yaml conf_text
-bw = conf[:amount] * (2 + conf[:key_size] + 2 + conf[:value_size])
 
-puts "-" * width
-puts conf_text
-puts "-" * width
+kv = Set(Ph::KV).new Array(Ph::KV).new conf[:amount] { {Random::DEFAULT.random_bytes(16), Random::DEFAULT.random_bytes(32)} }
 
-kv = Hash(Bytes, Bytes).new
-conf[:amount].times { kv[Random::DEFAULT.random_bytes(16)] = Random::DEFAULT.random_bytes(32) }
 tw = Time.measure do
-  kv.each { |k, v| ph.tx.set(k, v).commit }
+  kv.each { |k, v| env.tx.insert(k, v).commit }
 end
 tr = Time.measure do
   env = Ph::Env.from_yaml env_text
 end
-tc = Time.measure do
-  ph.checkpoint
-end
-ks = kv.keys
-ks.shuffle!
-ph.sst.stats.reset
-tg = Time.measure do
-  ks.each { |k| ph.get k }
-end
 
-{"write"      => tw,
- "recover"    => tr,
- "checkpoint" => tc,
- "get"        => tg,
+{"write"   => tw,
+ "recover" => tr,
 }.each do |o, tt|
   puts "#{o}:"
-  puts "\t#{(bw / tt.total_seconds).to_u64.humanize_bytes}/s"
-  puts "\t#{(conf[:amount] / tt.total_seconds).to_u64.humanize}r/s"
+  puts "\t#{(kv.size / tt.total_seconds).to_u64.humanize}r/s"
   puts "\t#{tt.total_seconds.humanize}s passed"
 end
-
-ph.sst.stats.reset
-ph.get kv.keys.sort.last
-puts({"last key search" => ph.sst.stats}.to_yaml)
-
-ph.sst.stats.reset
-ph.get kv.keys.sort.first
-puts({"first key search" => ph.sst.stats}.to_yaml)
-
-puts "#{bw}B (#{bw.humanize_bytes}) written"
-puts "-" * width
