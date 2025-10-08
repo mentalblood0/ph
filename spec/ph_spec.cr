a79ed993497d4f582f124ec16e7f0721fc5c17cd
mentalblood0 <necheporenko.stepan@yandex.ru>
2025-10-08 10:37:34 +0300

    fix some edge cases

--- spec/ph_spec.cr
+++ spec/ph_spec.cr
@@ -24,10 +24,10 @@ describe Ph::Sds, focus: true do
   sds = Ph::Sds.new 4_u8, 5_u8
 
   it "splits correctly" do
-    (1_u64..1024).each do |n|
+    (1_u64..2**20).each do |n|
+      # (UInt64::MAX // 2 - 1024 * 10..UInt64::MAX // 2).each do |n|
       fs = sds.fast_split n
-      # s = (sds.split n).reverse
-      # fs.should eq s
+      (fs.sum >= n).should eq true
     end
   end
 end
--- src/Sds.cr
+++ src/Sds.cr
@@ -7,12 +7,12 @@ module Ph
   class Sds
     Ph.mserializable
 
-    getter dss : UInt8
-    getter ps : UInt8
+    getter data_size_size : UInt8
+    getter pointer_size : UInt8
 
-    getter hs : UInt64 { @dss + @ps }
+    getter header_size : UInt64 { @data_size_size + @pointer_size }
 
-    def initialize(@dss, @ps)
+    def initialize(@data_size_size, @pointer_size)
     end
 
     def fast_split(n : UInt64)
@@ -22,7 +22,7 @@ module Ph
       (fbi + 1..63).each do |i|
         if 1 == n.bit i
           b = 1_u64 << i
-          if @ps * a.size >= b - asum
+          if a.size * @pointer_size >= b - asum
             a = [b * 2]
             asum = b * 2
           else
@@ -32,51 +32,6 @@ module Ph
         end
       end
       a
-    end
-
-    def split(n : UInt64)
-      dp = Array(UInt64).new(n + 1, UInt64::MAX)
-      dp[0] = 0_u64
-
-      used = Array(Array(UInt64)).new(n + 1) { [] of UInt64 }
-
-      pow = (Math.log n, 2).ceil.to_i32
-      while pow >= 0
-        b = 1_u64 << pow
-        (n).downto(0) do |c|
-          next if dp[c] == UInt64::MAX
-
-          max_possible = {c + b, n}.min
-
-          (c + 1..max_possible).each do |new|
-            overhead = if new <= b
-                         b - new
-                       else
-                         b - (new - c)
-                       end
-            overhead = 0 if overhead < 0
-
-            total_cost = dp[c] + overhead + 5
-
-            if total_cost < dp[new]
-              dp[new] = total_cost
-              used[new] = used[c] + [b]
-            end
-          end
-        end
-        pow -= 1
-      end
-
-      used[n]
-    end
-
-    def body_cost(s : Array(UInt64))
-      (((s.size == 1) ? 0 : s.size) * @ps + # blocks pointers
-        s.sum).to_u64!                      # blocks
-    end
-
-    def cost(s : Array(UInt64)) : UInt64
-      hs + body_cost s
     end
   end
 end
